740663e5cade8fd502d57310d4eda7d51adda034
Samrood Ali <devsamrood@gmail.com>
2025-09-28 13:45:36 +0530

    feat: order todos by id

--- db/actions/todos.ts
+++ db/actions/todos.ts
@@ -5,7 +5,7 @@ import { db } from "@/db/index";
 import { todo } from "@/db/schema";
 
 export const getData = async () => {
-  const data = await db.select().from(todo);
+  const data = await db.select().from(todo).orderBy(todo.id);
   return data;
 };
 
